b975bddc3ecef9731c5b7e35dac38b2c49675759
Charles Reace <cwreace@yahoo.com>
2016-09-02 19:05:31 -0400

    convert double-hypen to mdash

--- views/post.php
+++ views/post.php
@@ -7,7 +7,15 @@ require __DIR__.'/head.php';
 if(!empty($data['current'])) {
     require_once __DIR__.'/../parsedown/Parsedown.php';
     $parser = new Parsedown();
-    echo $parser->parse(file_get_contents(__DIR__.'/../posts/'.$data['current']['path'].'.md'));
+    echo str_replace(
+        '<img',
+        '<img class="hidden-xs"',
+        str_replace(
+            ' -- ',
+            '&thinsp;&mdash;&thinsp;',
+            $parser->parse(file_get_contents(__DIR__.'/../posts/'.$data['current']['path'].'.md'))
+        )
+    );
     echo <<<EOD
 <div
   class="fb-like"
